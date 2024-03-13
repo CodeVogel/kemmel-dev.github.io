b89b885bd1b87604fe6b1a3b9173b07b88479cb8
Kamiel de Visser <kamieldevisser@gmail.com>
2024-03-13 18:22:07 +0100

    add tailwind and init layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+	import NavBar from '$lib/NavBar.svelte';
+	import Footer from '$lib/Footer.svelte';
+</script>
+
+<main>
+	<NavBar />
+	<slot />
+	<Footer />
+</main>
