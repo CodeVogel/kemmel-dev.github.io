fbf932985d4745ff6548b8e6035e23660b583d14
Kamiel de Visser <kamieldevisser@gmail.com>
2024-03-14 12:53:21 +0100

    add shadcn-svelte button

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -1,1 +1,5 @@
-<nav>Nav</nav>
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
+<nav><Button variant="link">Click me!</Button></nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	import '../app.css';
+	import '../app.pcss';
 	import NavBar from '$lib/NavBar.svelte';
 	import Footer from '$lib/Footer.svelte';
 </script>
 
-<main class="h-full text-white bg-background">
+<main class="h-full dark bg-background">
 	<NavBar />
 	<slot />
 	<Footer />
