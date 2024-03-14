d1463e0da066e4d1b2ad94657505db9e638161cf
Kamiel de Visser <kamieldevisser@gmail.com>
2024-03-14 16:24:14 +0100

    basic layout navbar

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -5,16 +5,16 @@
 	import Button from '$lib/components/ui/button/button.svelte';
 </script>
 
-<nav class="flex flex-grow h-full p-4 border-2 border-solid border-border bg-secondary">
-	<div class="flex m-auto basis-1/4">
+<nav class="flex items-center h-full border-b-2 border-solid border-border">
+	<div class="flex py-4 my-auto basis-1/4">
 		<Logo />
 	</div>
-	<div class="flex justify-center align-middle basis-1/2">
-		<Button variant="link">Link 1</Button>
-		<Separator orientation="vertical" />
-		<Button variant="link">Link 2</Button>
+	<div class="flex justify-center basis-1/2">
+		<Button variant="link" class="flex h-full px-8 rounded-none hover:bg-accent">Link 1</Button>
+		<Separator orientation="vertical" class="h-10" />
+		<Button variant="link" class="flex h-full px-8 rounded-none hover:bg-accent">Link 2</Button>
 	</div>
-	<div class="flex justify-end align-middle basis-1/4">
+	<div class="flex justify-end px-8 basis-1/4">
 		<ThemeButton on:toggleTheme />
 	</div>
 </nav>
