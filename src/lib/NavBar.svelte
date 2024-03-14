ec0d91ba03705e51d405e1f418bab468496ddbbc
Kamiel de Visser <kamieldevisser@gmail.com>
2024-03-14 14:45:59 +0100

    add basic layout for navbar

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
-	import ThemeButton from './components/ui/button/ThemeButton.svelte';
+	import Logo from '$lib/components/ui/Logo.svelte';
+	import ThemeButton from '$lib/components/ui/button/ThemeButton.svelte';
+	import { Separator } from '$lib/components/ui/separator';
+	import Button from '$lib/components/ui/button/button.svelte';
 </script>
 
-<nav class="flex">
-	<ThemeButton on:toggleTheme />
+<nav class="flex flex-grow h-full p-4 border-2 border-solid border-border bg-secondary">
+	<div class="flex m-auto basis-1/4">
+		<Logo />
+	</div>
+	<div class="flex justify-center align-middle basis-1/2">
+		<Button variant="link">Link 1</Button>
+		<Separator orientation="vertical" />
+		<Button variant="link">Link 2</Button>
+	</div>
+	<div class="flex justify-end align-middle basis-1/4">
+		<ThemeButton on:toggleTheme />
+	</div>
 </nav>
