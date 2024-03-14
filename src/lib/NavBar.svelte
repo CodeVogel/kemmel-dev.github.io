66f4faa58983289893d6d674bff7e4b23ae900bf
Kamiel de Visser <kamieldevisser@gmail.com>
2024-03-14 13:34:49 +0100

    mv ThemeButton to own component

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -1,24 +1,7 @@
 <script lang="ts">
-	import Sun from 'svelte-radix/Sun.svelte';
-	import Moon from 'svelte-radix/Moon.svelte';
-	import { Button } from '$lib/components/ui/button/index.js';
-
-	import { createEventDispatcher } from 'svelte';
-	const dispatch = createEventDispatcher();
-
-	function toggleTheme() {
-		dispatch('toggleTheme');
-	}
+	import ThemeButton from './components/ui/button/ThemeButton.svelte';
 </script>
 
-<nav>
-	<Button on:click={toggleTheme} variant="outline" size="icon">
-		<Sun
-			class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-		/>
-		<Moon
-			class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-		/>
-		<span class="sr-only">Toggle theme</span>
-	</Button>
+<nav class="flex">
+	<ThemeButton on:toggleTheme />
 </nav>
