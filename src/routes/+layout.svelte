0ca2d7f059a47b33804ace55c522cd445c4ac4b9
Kamiel de Visser <kamieldevisser@gmail.com>
2024-03-14 13:16:59 +0100

    add modewatcher

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
+	import { ModeWatcher, toggleMode } from 'mode-watcher';
 	import '../app.pcss';
 	import NavBar from '$lib/NavBar.svelte';
 	import Footer from '$lib/Footer.svelte';
 </script>
 
-<main class="h-full dark bg-background">
-	<NavBar />
+<ModeWatcher defaultMode={'dark'} />
+<main class="h-full bg-background">
+	<NavBar on:toggleTheme={toggleMode} />
 	<slot />
 	<Footer />
 </main>
