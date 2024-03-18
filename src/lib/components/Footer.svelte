7d01b15ff940aee7f29b9eaab8709118929c616d
Kamiel de Visser <kamieldevisser@gmail.com>
2024-03-18 20:39:47 +0100

    update footer styling

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,4 @@
-<footer class="h-32 bg-gradient-to-t from-black/60 from-0% to-transparent to-100%">
+<footer class="h-32 text-foreground border-t-[1px] border-accent border-opacity-50">
 	<div class="flex items-center justify-center h-full gap-x-8">
 		<a>Link 1</a>
 		<a>Link 2</a>
