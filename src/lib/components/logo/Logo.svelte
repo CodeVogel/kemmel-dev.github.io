2a1a6c434d8e374331cf071d8459e2b416157d35
Kamiel de Visser <kamieldevisser@gmail.com>
2024-03-19 14:21:10 +0100

    redo logo

--- src/lib/components/logo/Logo.svelte
+++
@@ -1,9 +0,0 @@
-<div class="flex flex-row items-center text-3xl text-white" style="width: 12ch">
-	>&nbsp
-	<div class="typewriter">CodeVogel&nbsp<span class="emoji">🐦</span></div>
-	<span class="caret">_</span>
-</div>
-
-<style>
-	@import './typewriter.pcss';
-</style>
