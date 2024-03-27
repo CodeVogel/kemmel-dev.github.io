20e5b2a2093824b28d313812c61042a5b515dde8
Kamiel de Visser <kamieldevisser@gmail.com>
2024-03-27 17:15:39 +0100

    mv background-gradient to tailwind component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,8 +5,8 @@
 	import 'iconify-icon';
 </script>
 
-<main class="flex flex-col background-gradient text-white h-[100vh]">
-	<NavBar class="h-[6rem] fixed  background-gradient" />
+<main class="flex flex-col text-white background-gradient">
+	<NavBar class="h-[6rem] fixed background-gradient" />
 	<div class="min-h-[calc(100dvh-6rem-6rem)] mt-[6rem]">
 		<slot />
 	</div>
@@ -18,9 +18,5 @@
 <style>
 	:global(body, main) {
 		@apply min-h-[100vh];
-	}
-
-	.background-gradient {
-		@apply bg-fixed bg-gradient-to-b from-background via-70% via-background to-accent;
 	}
 </style>
