4d3410c82daf6ef58d7c9342390a772b2a1917a3
Kamiel de Visser <kamieldevisser@gmail.com>
2024-03-30 13:51:09 +0100

    update waveborder colors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,8 +7,8 @@
 	import WaveBorder from '$lib/components/WaveBorder.svelte';
 </script>
 
-<main class="flex flex-col text-white bg-background overflow-x-clip">
-	<NavBar class="h-[6rem] fixed bg-background" />
+<main class="z-10 flex flex-col text-white overflow-x-clip">
+	<NavBar class="h-[6rem] fixed bg-background-800" />
 	<WaveBorder class="mt-[6rem] fixed h-[4rem]" />
 	<div class="flex flex-col min-h-[calc(100dvh-10rem-4rem)] mt-[10rem]">
 		<slot />
@@ -20,6 +20,10 @@
 </main>
 
 <style>
+	:global(html, body) {
+		@apply bg-background-950;
+	}
+
 	:global(body, main) {
 		@apply min-h-[100vh];
 	}
