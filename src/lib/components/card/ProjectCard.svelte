90cc3e9b58fbdfa6112e05a284c5587ddb58bea8
Kamiel de Visser <kamieldevisser@gmail.com>
2024-04-01 13:00:57 +0200

    add ProjectCard

---
+++ src/lib/components/card/ProjectCard.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let project: ProjectDetail | null = null;
+</script>
+
+{#if project !== null}
+	<div class="bg-red-400 p8">
+		<h1>{project.title}</h1>
+		<p>{project.description}</p>
+	</div>
+{/if}
