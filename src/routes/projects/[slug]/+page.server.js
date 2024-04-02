a3b375a2c5e3b4046f1b55c9992e352338fa9ac8
Kamiel de Visser <kamieldevisser@gmail.com>
2024-04-02 14:43:55 +0200

    add entries for prerendering

---
+++ src/routes/projects/[slug]/+page.server.js
@@ -0,0 +1,10 @@
+/** @type {import('./$types').EntryGenerator} */
+export function entries() {
+	return [
+		{ slug: 'detective-duck' },
+		{ slug: 'detective-duck2' },
+		{ slug: 'detective-duck3' }
+	];
+}
+
+export const prerender = true;
