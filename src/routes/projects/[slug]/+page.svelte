1eba0183a905ff83563dad3a5ad7c1d5b5fa6589
Kamiel de Visser <kamieldevisser@gmail.com>
2024-04-04 19:20:24 +0200

    style .svx post

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -1,8 +1,42 @@
 <script lang="ts">
-	export let data;
+	export let data: import('./$types').PageData;
 </script>
 
-<div class="flex justify-center w-full">
-	{data.meta.title}
-	<svelte:component this={data.content} />
+<div class="flex justify-center w-full px-6 py-4">
+	<div class="flex flex-col max-w-[64ch] w-full">
+		<span class="title">{data.meta.title}</span>
+		<div id="post-container" style="--start: 0">
+			<svelte:component this={data.content} />
+		</div>
+	</div>
 </div>
+
+<style>
+	.title {
+		@apply text-5xl font-bold;
+	}
+
+	:global(#post-container .toc::before) {
+		content: 'Table of Contents';
+		@apply text-xl font-semibold;
+	}
+
+	:global(#post-container .shiki) {
+		@apply overflow-x-scroll;
+	}
+
+	:global(#post-container code) {
+		counter-reset: step;
+		counter-increment: step calc(var(--start, 1) - 1);
+	}
+
+	:global(#post-container code .line::before) {
+		content: counter(step);
+		counter-increment: step;
+		width: 1rem;
+		margin-right: 1.5rem;
+		display: inline-block;
+		text-align: right;
+		color: rgba(115, 138, 148, 0.4);
+	}
+</style>
