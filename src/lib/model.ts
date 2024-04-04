bb34d67fe7ebcdc475acf73f03cf03df4b0b3a5e
Kamiel de Visser <kamieldevisser@gmail.com>
2024-04-04 19:19:16 +0200

    add missingPostProps to model.ts

--- src/lib/model.ts
+++ src/lib/model.ts
@@ -1,10 +1,27 @@
-export type Categories = 'project' | 'blog';
+export type PostCategory = 'project' | 'blog';
 
 export type Post = {
 	title: string;
 	slug: string;
 	description: string;
 	date: string;
-	categories: Categories[];
+	category: PostCategory[];
 	published: boolean;
+	headerImage: string;
+	youtubeURL?: string;
 };
+
+// This function should return true if the data contains 
+// all non-optional properties of a post (minus the slug).
+export function missingPostProps(frontMatter: any) {
+	const requiredProperties = [
+		'title', 
+		'description', 
+		'date', 
+		'category', 
+		'published', 
+		'headerImage'
+	];
+	const missingProperties = requiredProperties.filter(prop => frontMatter[prop] === undefined || frontMatter[prop] === null)
+	return missingProperties;
+}
