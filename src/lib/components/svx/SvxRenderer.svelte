53a9f1b1ee12a53344ca282b4a66191970ca8099
Kamiel de Visser <kamieldevisser@gmail.com>
2024-04-07 15:52:10 +0200

    style SvxRenderer

--- src/lib/components/svx/SvxRenderer.svelte
+++ src/lib/components/svx/SvxRenderer.svelte
@@ -36,3 +36,44 @@
 		</div>
 	{/if}
 {/await}
+
+<style lang="scss">
+	:global {
+		.post-container {
+			.toc {
+				@apply justify-self-center px-8 py-4 rounded-3xl text-accent bg-primary border-black border-double border-[5px] my-4;
+			}
+
+			.toc::before {
+				content: 'Table of Contents';
+				@apply text-xl font-extrabold;
+			}
+
+			.toc ol {
+				@apply list-decimal;
+			}
+
+			.toc .toc-item::before {
+			}
+
+			.shiki {
+				@apply overflow-x-scroll;
+			}
+
+			code {
+				counter-reset: step;
+				counter-increment: step calc(var(--start, 1) - 1);
+
+				.line::before {
+					content: counter(step);
+					counter-increment: step;
+					width: 1rem;
+					margin-right: 1.5rem;
+					display: inline-block;
+					text-align: right;
+					color: rgba(115, 138, 148, 0.4);
+				}
+			}
+		}
+	}
+</style>
