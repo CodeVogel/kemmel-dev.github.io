8a914ac23dcf5efb5f78978f5e180c0769dc32f0
Kamiel de Visser <kamieldevisser@gmail.com>
2024-04-10 16:47:30 +0200

    add WorkExperience.svelte

---
+++ src/lib/components/ui/home/WorkExperience.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import ExperienceCard from '../card/ExperienceCard.svelte';
+	import type { WorkExperience } from '$lib/model';
+
+	import { onMount } from 'svelte';
+	let workExperience: WorkExperience[] = [];
+
+	onMount(async () => {
+		const response = await fetch('/src/content/experience.json');
+		workExperience = await response.json();
+	});
+</script>
+
+<div class="max-w-4xl p-4 m-8 bg-background-100/25 rounded-3xl">
+	<h1 class="mb-8">Experience</h1>
+	{#each workExperience as experience}
+		<ExperienceCard {experience}></ExperienceCard>
+	{/each}
+</div>
