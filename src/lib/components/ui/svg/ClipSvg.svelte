beb130a1b3f84215b1fc0eb7ed8318241dc035b6
Kamiel de Visser <kamieldevisser@gmail.com>
2024-04-10 16:47:17 +0200

    add ClipSvg.svelte

---
+++ src/lib/components/ui/svg/ClipSvg.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	let className: string;
+	export let path: string;
+	export { className as class };
+</script>
+
+<div class={className} style="mask:url({path}) no-repeat center"></div>
