752e93a57adf53fdab21e4f3b3beb8963eb34c0d
Kamiel de Visser <kamieldevisser@gmail.com>
2024-04-10 22:01:12 +0200

    update ExperienceCard

--- src/lib/components/ui/card/ExperienceCard.svelte
+++ src/lib/components/ui/card/ExperienceCard.svelte
@@ -6,17 +6,32 @@
 </script>
 
 <div
-	class="grid grid-flow-row-dense p-4 mb-4 border-2 shadow-md rounded-xl gap-x-8 gap-y-4 bg-gradient-to-b from-background-300 to-background-500 border-accent-400"
+	class="flex flex-col p-4 mb-4 border-2 shadow-md sm:flex-row rounded-xl gap-x-8 gap-y-4 bg-gradient-to-br from-background-300 to-background-800 border-accent-400"
 >
-	<a class="hover:text-primary [&>div]:hover:bg-primary" href={experience.company.website}>
-		<ClipSvg path={experience.company.logo} class="bg-white mt-[.3lh] w-32 h-8"></ClipSvg>
+	<a class="grid items-center [&_div]:hover:bg-primary basis-1/6" href={experience.company.website}>
+		<ClipSvg
+			path={experience.company.logo}
+			class="flex items-center justify-center w-24 h-16 bg-white"
+		></ClipSvg>
 	</a>
-	<div class="flex flex-col">
-		<h2 class="mb-2 font-mono text-primary">{experience.position}</h2>
+	<div class="flex flex-col basis-3/6">
+		<h1 class="mb-2 font-mono text-primary">{experience.position}</h1>
 		<i>{experience.description}</i>
 	</div>
-	<div class="flex flex-col text-white/75">
-		<p>{experience.location}</p>
-		<p>{experience.timeSpan.startDate} - {experience.timeSpan.endDate}</p>
+	<div class="flex justify-end flex-grow">
+		<div class="grid grid-cols-[min-content,min-content] gap-x-4">
+			<span><iconify-icon icon="mdi-location"></iconify-icon></span>
+			<p>{experience.location}</p>
+			<p>From:</p>
+			<p>{experience.timeSpan.startDate}</p>
+			<p>To:</p>
+			<p>{experience.timeSpan.endDate}</p>
+		</div>
 	</div>
 </div>
+
+<style lang="scss">
+	h1 {
+		@apply text-lg;
+	}
+</style>
