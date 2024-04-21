1bd3a2c0c6c39e14f6167d083340d821842d5917
Kamiel de Visser <kamieldevisser@gmail.com>
2024-04-21 22:53:37 +0200

    set new margin on WorkExperience

--- src/lib/components/ui/home/WorkExperience.svelte
+++ src/lib/components/ui/home/WorkExperience.svelte
@@ -5,11 +5,11 @@
 	export let workExperience: WorkExperience[];
 </script>
 
-<div class="flex flex-col items-center mt-[25vh] mb-8 gap-y-8 w-full">
+<div class="flex flex-col items-center w-full mt-4">
 	<Viewnimation>
 		<h1 class="text-center">Experience</h1>
 	</Viewnimation>
-	<div class="grid items-center grid-cols-1 w-max-lg gap-y-8">
+	<div class="grid items-center grid-cols-1 w-max-lg">
 		{#each workExperience as experience, i}
 			<Viewnimation fromLeft={i % 2 !== 0}>
 				<ExperienceCard {experience}></ExperienceCard>
