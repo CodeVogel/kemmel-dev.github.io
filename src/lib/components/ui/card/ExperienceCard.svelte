7f1f67380152a308558ab7495611a5edfb3d8e44
Kamiel de Visser <kamieldevisser@gmail.com>
2024-04-21 17:36:06 +0200

    remove card look from logo

--- src/lib/components/ui/card/ExperienceCard.svelte
+++ src/lib/components/ui/card/ExperienceCard.svelte
@@ -6,10 +6,7 @@
 </script>
 
 <div class="grid grid-cols-[min-content,auto] gap-x-4 mx-2 max-w-lg">
-	<a
-		class="[&_div]:hover:bg-primary bg-gradient-to-t from-black to-accent-500 shadow-xl shadow-black/80 hover:bg-primary w-32 p-4 rounded-3xl"
-		href={experience.company.website}
-	>
+	<a class="[&_div]:hover:bg-primary hover:bg-primary w-32 p-4" href={experience.company.website}>
 		<ClipSvg path={experience.company.logo} class="bg-white aspect-square"></ClipSvg>
 	</a>
 	<div class="flex flex-col justify-center w-full">
