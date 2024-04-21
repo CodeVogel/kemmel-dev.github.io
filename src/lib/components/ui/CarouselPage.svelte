fb6ce5cc73f52fc23591cab398354560e469024a
Kamiel de Visser <kamieldevisser@gmail.com>
2024-04-21 22:48:02 +0200

    extract carousel component

---
+++ src/lib/components/ui/CarouselPage.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<div class="carousel-item h-page overflow-clip">
+	<slot />
+</div>
+
+<style>
+	.carousel-item {
+		@apply box-content flex flex-none;
+		scroll-snap-align: start;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,33 @@
 	import BirdHero from '../lib/components/ui/home/BirdHero.svelte';
 	import HeroCard from '$lib/components/ui/home/WorkExperience.svelte';
 	import Viewnimation from '$lib/components/ui/polish/viewnimation/Viewnimation.svelte';
+	import CarouselPage from '$lib/components/ui/CarouselPage.svelte';
 
 	export let data: import('./$types').PageServerData;
 </script>
 
-<div class="flex flex-col items-center">
-	<Viewnimation>
-		<BirdHero />
-	</Viewnimation>
-	<HeroCard workExperience={data.workExperiences} />
+<div class="h-page carousel carousel-vertical">
+	<CarouselPage>
+		<div class="flex flex-col items-center w-full">
+			<Viewnimation>
+				<BirdHero />
+			</Viewnimation>
+		</div>
+	</CarouselPage>
+	<CarouselPage>
+		<HeroCard workExperience={data.workExperiences} />
+	</CarouselPage>
 </div>
+
+<style>
+	.carousel {
+		@apply inline-flex overflow-x-scroll;
+		scroll-snap-type: x mandatory;
+		scroll-behavior: smooth;
+
+		&-vertical {
+			@apply flex-col overflow-y-scroll;
+			scroll-snap-type: y mandatory;
+		}
+	}
+</style>
