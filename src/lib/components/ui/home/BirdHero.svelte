47c2bc839426a60b3ea1365ebf83476cd154b8eb
Kamiel de Visser <kamieldevisser@gmail.com>
2024-06-01 21:56:31 +0200

    refactor: change layout

--- src/lib/components/ui/home/BirdHero.svelte
+++ src/lib/components/ui/home/BirdHero.svelte
@@ -3,39 +3,42 @@
    import Logo from '$lib/components/ui/polish/logo/Logo.svelte';
 </script>
 
-<div class="flex flex-col items-center justify-end gap-y-8 min-h-[calc(8rem+10lh)] mt-16 lg:mt-32">
-   <div class="relative speech-bubble rounded-2xl bg-background-300">
-      <AutoGrowTypeWriter
-         class="p-6 rounded-lg shadow-xl bg-inherit shadow-black/80 min-w-80"
-         msDelayBetweenChars={20}
-         msDelayBetweenElements={350}
-         preRender={false}
-      >
-         <h1>Hi!</h1>
-         <h2>
-            <div class="max-w-[9ch] sm:max-w-full">
-               My name is
-               <span class="px-1 font-bold text-black rounded-lg w-fit bg-primary">
-                  CodeVogel
-               </span>.
+<div class="flex flex-col items-center">
+   <div class="min-h-[10lh] mt-8 content-end">
+      <div class="relative speech-bubble rounded-2xl bg-background-300">
+         <AutoGrowTypeWriter
+            class="p-8 rounded-lg shadow-xl bg-inherit shadow-black/80 min-w-[28ch]"
+            msDelayBetweenChars={20}
+            msDelayBetweenElements={500}
+            preRender={false}
+         >
+            <h1>Hi!</h1>
+            <h2>
+               <div class="max-w-[9ch] sm:max-w-full">
+                  My name is
+                  <span class="px-1 font-bold text-black rounded-lg w-fit bg-primary">
+                     CodeVogel
+                  </span>.
+               </div>
+            </h2>
+            <div class="mt-2 font-extralight">
+               I make <span class="text-primary">games</span> and build
+               <span class="text-primary">software</span>.
             </div>
-         </h2>
-         <div class="mt-2 font-extralight">
-            I make <span class="text-primary">games</span> and build
-            <span class="text-primary">software</span>.
-         </div>
-         <div class="font-extralight">
-            Check out some of my
-            <a href="/projects" class="font-normal underline text-primary"
-               ><span
-                  >projects<iconify-icon class="ms-1" icon="ion:open-outline"></iconify-icon></span
-               ></a
-            >
-            !
-         </div>
-      </AutoGrowTypeWriter>
+            <div class="font-extralight">
+               Check out some of my
+               <a href="/projects" class="font-normal underline text-primary"
+                  ><span
+                     >projects<iconify-icon class="ms-1" icon="ion:open-outline"
+                     ></iconify-icon></span
+                  ></a
+               >
+               !
+            </div>
+         </AutoGrowTypeWriter>
+      </div>
    </div>
-   <Logo class="w-[8rem] shadow-[5px_6px_15px_2px_#000000aa] rounded-[100vw]" />
+   <Logo class="w-[8rem] mt-8 shadow-[5px_6px_15px_2px_#000000aa] rounded-[100vw]" />
 </div>
 
 <style>
