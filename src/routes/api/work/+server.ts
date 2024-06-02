522860187dd2a54770064b37c846f4f87e30d496
Kamiel de Visser <kamieldevisser@gmail.com>
2024-06-02 13:16:50 +0200

    refactor: add types

--- src/routes/api/work/+server.ts
+++ src/routes/api/work/+server.ts
@@ -1,7 +1,9 @@
+import type { RequestEvent } from '@sveltejs/kit';
 import { json } from '@sveltejs/kit';
 
+
 // GET /api/work
-export async function GET({ fetch }) {
+export async function GET({ fetch }: RequestEvent) {
     const response = await fetch('/experience.json');
     const workExperiences = await response.json();
     return json(workExperiences);
