03968909b5fcae32f939bbf02bce89298f27f369
Kamiel de Visser <kamieldevisser@gmail.com>
2024-06-15 15:04:47 +0200

    re-order tw classes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
    import Viewnimation from '$lib/components/ui/polish/viewnimation/Viewnimation.svelte';
    import { onMount } from 'svelte';
    import Skills from '$lib/components/ui/home/Skills.svelte';
+
    export let data: import('./$types').PageServerData;
 
    onMount(() => {
@@ -23,7 +24,7 @@
       icon="mdi:arrow-bottom-circle"
       class="fixed bottom-6 right-6 text-6xl text-primary/50"
    ></iconify-icon>
-   <div class="flex flex-col items-center w-full min-h-[calc(100dvh-theme(spacing.nav))]">
+   <div class="flex min-h-[calc(100dvh-theme(spacing.nav))] w-full flex-col items-center">
       <Viewnimation>
          <BirdHero />
       </Viewnimation>
