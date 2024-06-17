c0a882f686aee6b7ff2b025605a85f63cca958c7
Kamiel de Visser <kamieldevisser@gmail.com>
2024-06-17 12:53:04 +0200

    add scroll indicator delay

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,21 +6,28 @@
    import WorkExperience from '$lib/components/ui/home/WorkExperience.svelte';
 
    onMount(() => {
-      // on scroll, change the color of the icon
-      window.addEventListener('scroll', () => {
-         let scrollIndicator = document.querySelector('#scroll-indicator') as HTMLElement | null;
-         if (scrollIndicator) {
+      const scrollIndicator = document.querySelector('#scroll-indicator') as HTMLElement | null;
+
+      if (scrollIndicator) {
+         window.addEventListener('scroll', () => {
             scrollIndicator.style.opacity = `${1 - scrollY / 200}`;
+         });
+      }
+
+      // after delay, show scroll indicator
+      setTimeout(() => {
+         if (scrollIndicator) {
+            scrollIndicator.classList.remove('hidden');
          }
-      });
+      }, 2000);
    });
 </script>
 
 <div class="grid grid-cols-1 gap-y-8">
    <iconify-icon
       id="scroll-indicator"
-      icon="mdi:arrow-bottom-circle"
-      class="fixed bottom-6 right-6 text-6xl text-primary/50"
+      icon="line-md:arrow-down"
+      class="fixed bottom-6 right-6 hidden text-4xl text-white/50"
    ></iconify-icon>
    <div class="flex min-h-[calc(100dvh-theme(spacing.nav))] w-full flex-col items-center">
       <Viewnimation>
