16f3d85da01e18accb3612f75c6e1d97fe287813
Kamiel de Visser <kamieldevisser@gmail.com>
2024-06-18 00:08:23 +0200

    change contact page text

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -16,14 +16,14 @@
             I am currently <strong class="text-primary">open to work</strong>.
          </div>
          <div>Are you interested in working together?</div>
-         <div>Then don't hesitate to shoot me an email at:</div>
+         <div>Then I would love to hear from you!<br /> Don't hesitate to contact me at:</div>
          <span class="my-4 rounded-lg bg-primary px-4 pb-3 pt-2 text-xl font-extrabold text-black"
             >contact<span class="hidden">primitive-spam-protection</span>@codevogel.com</span
          >
          <div>Other inquiries, such as questions about one of my projects, are also welcome!</div>
       </div>
 
-      <h2>Socials</h2>
+      <h3 class="mb-4 italic text-white/50">You can also find me on...</h3>
       <IconBox icons={linkIcons}></IconBox>
    </div>
 </div>
