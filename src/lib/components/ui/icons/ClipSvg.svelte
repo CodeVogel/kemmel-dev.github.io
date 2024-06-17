1ff05d7dda5fa54901266f8da66fe619ca116253
Kamiel de Visser <kamieldevisser@gmail.com>
2024-06-18 00:08:40 +0200

    reorder tw classes

--- src/lib/components/ui/icons/ClipSvg.svelte
+++ src/lib/components/ui/icons/ClipSvg.svelte
@@ -5,5 +5,5 @@
 </script>
 
 <div class={className} style="mask:url({path}) no-repeat center">
-   <img src={path} class="opacity-0 aspect-square" width="100vh" height="100vh" />
+   <img src={path} class="aspect-square opacity-0" width="100vh" height="100vh" />
 </div>
