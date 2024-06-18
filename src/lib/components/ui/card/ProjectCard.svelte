687aee0b4d2a12e7d78c7352caad89c97e09d4d9
Kamiel de Visser <kamieldevisser@gmail.com>
2024-06-18 17:56:26 +0200

    add youtube preview to projectcard

--- src/lib/components/ui/card/ProjectCard.svelte
+++ src/lib/components/ui/card/ProjectCard.svelte
@@ -8,11 +8,25 @@
    href="/projects/{post.slug}"
    class="flex flex-col gap-y-2 rounded-xl border-[2px] border-accent/75 bg-gradient-to-b from-background-800 via-background-800 via-40% to-background-500 px-4 py-4 shadow-[0px_0px_12px_12px_rgba(0,0,0,0.3)] shadow-black/50 transition-all duration-300 hover:shadow-white/20 [&_h1]:hover:text-xl [&_iconify-icon]:hover:text-4xl [&_iconify-icon]:hover:text-primary [&_p]:hover:text-sm"
 >
-   <div
-      class="flex h-48 flex-grow flex-col bg-cover bg-center"
-      style="background-image:url(/headers/{post.slug}.png)"
-   >
-      <div class="mb-0 mt-auto grid grid-cols-5 bg-black/50 p-2">
+   <div class="flex h-64 flex-col">
+      {#if post.youtubeURL != undefined}
+         <iframe
+            class="flex flex-grow"
+            src="https://www.youtube-nocookie.com/embed/oIzACzUwmVM?si=xU_cO-6pQfrxR2pp&amp;controls=0"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+         ></iframe>
+      {:else}
+         <div
+            class="flex flex-grow"
+            style="background-image:url(/headers/{post.slug}.png); background-size: cover"
+         ></div>
+      {/if}
+
+      <div class="bottom-0 grid min-h-[3lh] grid-cols-5 bg-black/50 p-2">
          <div class="col-span-4">
             <h1>{post.title}</h1>
             <p>{post.description}</p>
