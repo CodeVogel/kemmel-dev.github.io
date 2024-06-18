f95ea42437d8256da2942fe4ed4aa84fc01da834
Kamiel de Visser <kamieldevisser@gmail.com>
2024-06-18 22:13:08 +0200

    add business card route at /card

---
+++ src/routes/card/+page.svelte
@@ -0,0 +1,7 @@
+<div class="flex flex-col text-center">
+   <h1 class="my-4">Nice to meet you!</h1>
+   <h3>Save my website for later:</h3>
+   <div class="mt-8 flex flex-row justify-center">
+      <img class="w-[60%] max-w-[32rem]" src="/qr.png" alt="QR code" />
+   </div>
+</div>
